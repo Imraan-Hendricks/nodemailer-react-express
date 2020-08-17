d0b3d334dface4d80c4d28e918eaf7c5a873b8e6
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-18 01:22:31 +0200

    (server): set static directories

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
