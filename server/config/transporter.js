9bb468b9883e2c53c383532e67208deef2664e9b
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-18 08:52:42 +0200

    (server): added nodemailer transporter config

---
+++ server/config/transporter.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+const {
+  NODEMAILER_USER,
+  NODEMAILER_CLIENT_ID,
+  NODEMAILER_CLIENT_SECRET,
+  NODEMAILER_REFRESH_TOKEN,
+  NODEMAILER_ACCESS_TOKEN,
+} = require('./env');
+
+module.exports = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  secure: true,
+  auth: {
+    type: 'OAuth2',
+    user: NODEMAILER_USER,
+    clientId: NODEMAILER_CLIENT_ID,
+    clientSecret: NODEMAILER_CLIENT_SECRET,
+    refreshToken: NODEMAILER_REFRESH_TOKEN,
+    accessToken: NODEMAILER_ACCESS_TOKEN,
+  },
+});
