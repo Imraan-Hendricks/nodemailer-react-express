56bef49f11b6fe941939fd6d0caa6a2e7ec40137
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-19 12:00:07 +0200

    (server): added contact us route

--- server/config/env.js
+++ server/config/env.js
@@ -1,4 +1,5 @@
 exports.PORT = process.env.PORT || 5000;
+exports.MAIL_TO = process.env.MAIL_TO;
 exports.NODEMAILER_USER = process.env.NODEMAILER_USER;
 exports.NODEMAILER_CLIENT_ID = process.env.NODEMAILER_CLIENT_ID;
 exports.NODEMAILER_CLIENT_SECRET = process.env.NODEMAILER_CLIENT_SECRET;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const ApiRouter = express.Router();
+const { handle } = require('../utils');
+const { MAIL_TO } = require('../config/env');
+const transporter = require('../config/transporter');
 
-ApiRouter.get('/', (req, res) => {
-  res.json({ route: 'api' });
+ApiRouter.post('/contactUs', async (req, res) => {
+  const html = `<div><p>${req.body.message}</p><ul><li>firstName: ${req.body.firstName}</li><li>lastName: ${req.body.lastName}</li><li>contact: ${req.body.contact}</li><li>email: ${req.body.email}</li></ul></div>`;
+
+  const mailOptions = {
+    from: `${req.body.firstName} ${req.body.lastName} <${req.body.email}>`,
+    to: MAIL_TO,
+    replyTo: req.body.email,
+    subject: 'Enquiry',
+    html: html,
+  };
+
+  const [mailRes, err] = await handle(transporter.sendMail(mailOptions));
+
+  if (err) {
+    const err = [
+      {
+        location: 'nodemailer',
+        msg: 'Email failed. Please try again',
+        param: 'general',
+        value: '',
+      },
+    ];
+    return res.json({ success: false, err });
+  }
+
+  res.json({
+    success: true,
+    data: {
+      ...mailOptions,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      from: req.body.email,
+      message: req.body.message,
+    },
+  });
 });
 
 module.exports = ApiRouter;
