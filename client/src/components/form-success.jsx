4cb2a20d363752ca2928533bdc859fa814ef74d9
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-22 18:05:18 +0200

    (client): added form success component

--- client/src/components/form-success.jsx
+++ client/src/components/form-success.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import { useFormSuccessStyles } from '../styles/form-success-styles';
 
 export const FormSuccess = (props) => {
+  const classes = useFormSuccessStyles();
+
   return (
     <div>
       <h1>Success</h1>
-      <button onClick={() => props.setState('email')}>Back</button>
+      <Button
+        classes={{ contained: classes.button }}
+        variant='contained'
+        color='primary'
+        onClick={() => props.setState('email')}>
+        Back
+      </Button>
     </div>
   );
 };
